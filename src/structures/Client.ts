3fc1b22f07371d15bfe36c172e13f5497e111d0c
shersafi <sher.safi@ryerson.ca>
2021-06-22 16:22:00 -0400

    client fix

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -28,7 +28,10 @@ export default class LastyClient extends CommandoClient {
         commandState: false,
         unknownCommand: false
       })
-      .registerCommandsIn(path.join(__dirname, '../commands'));
+      .registerCommandsIn({
+        filter: /^([^.].*)\.(js|ts)$/,
+        dirname: path.join(__dirname, "commands"),
+      });
       
 
     this.on('ready', () => require('../events/ready')(this));
