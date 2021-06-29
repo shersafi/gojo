e79a05dc6c8d2cec709e0dd8e64b93d8b77f4f9e
shersafi <sher.safi@ryerson.ca>
2021-06-29 18:24:34 -0400

    clowncount

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,9 +1,10 @@
 import { MessageEmbed } from 'discord.js';
 import { TextChannel } from 'discord.js';
-import clown from '../../clown.json';
 import { CommandoClient } from 'discord.js-commando';
 import path from 'path';
 import { PREFIX,  DISCORD_BOT_TOKEN } from '../../config.json';
+
+import clowncount from '../clowncount.json';
 
 export default class LastyClient extends CommandoClient {
   public constructor() {
@@ -47,22 +48,28 @@ export default class LastyClient extends CommandoClient {
       
         if (reaction.emoji.name === '🤡') {
 
+          const clownCount = clowncount['count'] + 1;
+
+          clowncount['count'] = clownCount;
+
           const msg = reaction.message;
           const user = reaction.message.author;
           const url = reaction.message.url;
           const chan = reaction.message.channel;
           const clownboard = msg.guild?.channels.cache.find(channel => channel.id == '855944224071221258');
-          const count = clown.count + 1
+          
           const embed = new MessageEmbed()
             .setColor('#c9befb')
             .setAuthor(user.tag, user.displayAvatarURL())
             .setTimestamp()
-            .setFooter(`🤡 ${count} | ${reaction.message.id}`)
+            .setFooter(`🤡 ${clownCount} | ${reaction.message.id}`)
             .setDescription(reaction.message.content)
             .addField('#' + (chan as TextChannel).name, `[Jump to message](${url})`);
           
 
           (clownboard as TextChannel).send(embed);
+
+
 
           
         }
